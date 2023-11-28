be7d4d3fabe29a532262a12ea53ad79a79b6131d
IzaanAnwar <mdizaan67@gmail.com>
2023-11-28 22:35:34 +0530

    au

--- src/index.js
+++ src/index.js
@@ -11,13 +11,27 @@ app.use(express.static("public"));
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on("offer", (offer) => {
+    console.log("Received offer");
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    console.log("Received answer");
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("ice-candidate", (candidate) => {
+    console.log("Received ICE candidate");
+    socket.broadcast.emit("ice-candidate", candidate);
   });
 
   socket.on("message", (message) => {
     console.log("message:", message);
     io.emit("message", message); // Broadcast the message to all clients
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
