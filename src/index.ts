db04a6b046f02eac860b8d2f2e2a212d3f0fa4df
IzaanAnwar <mdizaan67@gmail.com>
2023-11-28 22:00:37 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("message", (message: any) => {
+    console.log("message:", message);
+    io.emit("message", message); // Broadcast the message to all clients
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
