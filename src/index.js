12502769d1148f589072f787b2daad951532f16c
IzaanAnwar <mdizaan67@gmail.com>
2023-12-04 15:06:50 +0530

    little mod

--- src/index.js
+++ src/index.js
@@ -1,39 +1,44 @@
 const express = require("express");
 const http = require("http");
-const socketIO = require("socket.io");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = new Server(server, { cors: true });
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log("A user connected", socket.id);
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
 
-  socket.on("createRoom", (roomName) => {
-    socket.join(roomName);
-    console.log(`User joined room: ${roomName}`);
+  socket.on("join-room", (data) => {
+    const { email, roomId } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(roomId).emit("user-joined", { email: email, id: socket.id });
+    socket.join(roomId);
+    io.to(socket.id).emit("join-room", data);
+    console.log(`[${email}] joined room: ${roomId}`);
   });
 
-  socket.on("offer", (data) => {
-    io.to(data.room).emit("offer", data.offer);
+  socket.on("call-user", (data) => {
+    const { to, offer } = data;
+    io.to(to).emit("incoming-call", { from: socket.id, offer });
   });
-
-  socket.on("answer", (data) => {
-    io.to(data.room).emit("answer", data.answer);
-  });
-
-  socket.on("iceCandidate", (data) => {
-    io.to(data.room).emit("iceCandidate", data.candidate);
+  socket.on("call-accepted", (data) => {
+    const { to, ans } = data;
+    io.to(to).emit("call-accepted", { from: socket.id, ans });
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
